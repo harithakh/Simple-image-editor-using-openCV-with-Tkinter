f36487d5cbc27ab3eec6878a941ff1ab33693682
harithakh <hkhulangamuwa@gmail.com>
2022-09-11 22:08:32 +0530

    upload_image function added.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("The Image Editor")
-window.minsize(width=800, height=600)
-window.config(padx=10, pady=10)
+window.minsize(width=1000, height=600)  # window size at start
+window.config(padx=10, pady=10)  # padding
 
-window.mainloop()
+
+def upload_image():
+    global img
+    file_types = [('jpeg Files', '*.jpeg'),  # these are the file types
+                  ('jpg Files', '*.jpg'),
+                  ('png Files', '*.png')]
+    file_name = filedialog.askopenfilename(title='Select an image',
+                                           filetypes=file_types)  # this returns the full file path
+    img = ImageTk.PhotoImage(file=file_name)
+    button_2 = tk.Button(window, image=img)
+    button_2.grid(row=1, column=1)
+
+
+button_1 = tk.Button(window, text="New", width=10, command=upload_image)
+button_1.grid(row=0, column=0)
+
+window.mainloop()  # loop to keep the window open
